e814190ff7f8ed5dc25de25f20eb3cdb391ffb22
Jozsef M Komjati <jm.komjati@codegarager.net>
2021-04-08 16:17:43 +0100

    Lesson 4 - The one about bubble sorting

--- jslesson_4/bubble.ts
+++ jslesson_4/bubble.ts
@@ -1,11 +1,16 @@
-export function bubbleSort(sortMe: number[]): number[] {
+function bubbleSort(sortMe: number[]): number[] {
   // create a new copy
   const sortMeCopy = [...sortMe];
 
   // outer loop
   for (let i = 0; i < sortMeCopy.length; i++) {
+    console.log(`_outer loop running for the ${i} time`);
     // inner loop
     for (let j = 0; j < sortMeCopy.length - 1; j++) {
+      console.log(`__inner loop running for the ${j} time`);
+      console.log(
+        `___inner loop compares ${sortMeCopy[j]} against ${sortMeCopy[j + 1]}`
+      );
       // condition / check
       if (sortMeCopy[j] > sortMeCopy[j + 1]) {
         // swap
---
+++ jslesson_4/sort-builtin.ts
@@ -0,0 +1,4 @@
+const unsorted0 = [1999, 100, 7, 67, 11, 1, 3];
+const sorted0 = unsorted0.sort((a, b) => a - b);
+
+console.log(sorted0);
